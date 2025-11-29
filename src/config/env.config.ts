024367fb74991c064db66e1adda6f19523ac7178
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-11-29 13:18:13 +0600

    Next js version upgrade to 16.0.5

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,9 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5000';
 const accessSecret = process.env.JWT_SECRET;
+const NODE = process.env.NODE_ENV;
 
 const envVars = {
+  NODE,
   baseUrl,
   jwt: {
     accessSecret,
