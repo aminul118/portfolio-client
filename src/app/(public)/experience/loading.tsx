354fd8194f0a407f47f654b9f3f2f69764a08cb4
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2026-02-14 18:21:35 +0600

    Experience Loading fix

--- src/app/(public)/experience/loading.tsx
+++ src/app/(public)/experience/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 const ExperienceLoading = () => {
   return (
-    <section className="container mx-auto px-4 py-16 lg:mt-28">
+    <section className="container mx-auto px-4 py-16">
       {/* Background Gradients */}
       <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
         <div className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" />
