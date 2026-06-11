7dc4da2a31871f93e665969c22660684857187e2
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2026-06-11 20:58:05 +0600

    Domain change

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -4,7 +4,7 @@ const baseCookieOption = {
   httpOnly: true,
   secure: envVars.nodeEnv === 'production',
   sameSite: 'lax',
-  domain: envVars.nodeEnv === 'production' ? '.aminuldev.site' : 'localhost',
+  domain: envVars.nodeEnv === 'production' ? '.aminul.site' : 'localhost',
   path: '/',
 } as const;
 
--- src/config/meta.config.ts
+++ src/config/meta.config.ts
@@ -1,21 +1,21 @@
 import { MetaConfig } from '@/types';
 
 const metaConfig: MetaConfig = {
-  baseUrl: 'https://www.aminuldev.site',
+  baseUrl: 'https://www.aminul.site',
   baseImage: '/og-image.png',
   siteName: 'Md. Aminul Islam Portfolio',
   category: 'portfolio',
   applicationName: 'Md. Aminul Islam Portfolio',
   facebook_app_id: '580317868506376',
   authors_name: 'Md. Aminul Islam',
-  authorPortfolio: 'http://www.aminuldev.site',
+  authorPortfolio: 'http://www.aminul.site',
   twitter_site: '@aminul118',
-  bookmarks: 'https://www.aminuldev.site/logo.png',
+  bookmarks: 'https://www.aminul.site/logo.png',
   verification: {
     google: 'xUHURPD29At_D9F66UvFk6hYJXWC6i0lmpjcVL4c_LA',
     microsoft_bing: 'EE37CB89A12B6DB168EE052A7D138295',
   },
-  publisher: 'https://www.aminuldev.site',
+  publisher: 'https://www.aminul.site',
   preventCrawler: ['/dashboard', '/admin'],
 };
 
